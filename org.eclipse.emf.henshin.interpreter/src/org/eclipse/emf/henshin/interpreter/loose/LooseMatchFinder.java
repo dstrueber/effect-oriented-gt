d5b12e1ad3c3f1d9473f40fe684e523598f01fd1
Daniel Strueber <strueber@mathematik.uni-marburg.de>
2023-01-20 16:15:56 +0100

    New implementing of matching with backtracking

--- org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/loose/LooseMatchFinder.java
+++ org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/loose/LooseMatchFinder.java
@@ -19,6 +19,7 @@ import org.eclipse.emf.henshin.interpreter.info.VariableInfo;
 import org.eclipse.emf.henshin.interpreter.matching.constraints.DanglingConstraint;
 import org.eclipse.emf.henshin.model.Attribute;
 import org.eclipse.emf.henshin.model.Edge;
+import org.eclipse.emf.henshin.model.Graph;
 import org.eclipse.emf.henshin.model.Node;
 import org.eclipse.emf.henshin.model.Parameter;
 import org.eclipse.emf.henshin.model.Rule;
@@ -27,14 +28,14 @@ public class LooseMatchFinder {
 	Rule rule;
 	EGraph graph;
 	Engine engineImpl;
-	Map<Parameter,Object> paramValues;
-	
+	Map<Parameter, Object> paramValues;
+
 	public LooseMatchFinder(Rule rule, EGraph graph, Engine engineImpl) {
 		super();
 		this.rule = rule;
 		this.graph = graph;
 		this.engineImpl = engineImpl;
-		this.paramValues = new HashMap<Parameter,Object>();
+		this.paramValues = new HashMap<Parameter, Object>();
 	}
 
 	/**
@@ -49,27 +50,31 @@ public class LooseMatchFinder {
 		SpecialRuleInfo kernelRuleInfo = KernelRuleHandler.createKernelRule(rule);
 		SpecialRuleInfo grayedRuleInfo = GrayedRuleHandler.createGrayedRule(rule);
 
-		Assignment assignment = KernelRuleHandler.convertParameterValues(kernelRuleInfo,paramValues);
-		Iterable<Match> kernelMatches = engineImpl.findMatches(kernelRuleInfo.getSpecialRule(), graph, new MatchImpl(assignment, false));
+		Assignment assignment = KernelRuleHandler.convertParameterValues(kernelRuleInfo, paramValues);
+		Iterable<Match> kernelMatches = engineImpl.findMatches(kernelRuleInfo.getSpecialRule(), graph,
+				new MatchImpl(assignment, false));
 		if (!kernelMatches.iterator().hasNext())
 			return null;
 
 		Match kernelMatch = kernelMatches.iterator().next();
 		Match originalMatch = KernelRuleHandler.translateMatchFromKernel(kernelRuleInfo, kernelMatch);
-		Match resultMatch = GrayedRuleHandler.translateMatchFromOriginal(grayedRuleInfo, originalMatch);
+		Match grayedMatch = GrayedRuleHandler.translateMatchFromOriginal(grayedRuleInfo, originalMatch);
 
-		List<Node> searchPlan = getUnboundNodes(resultMatch, grayedRuleInfo.getSpecialRule());
+		List<Node> unboundNodeList = getUnboundNodes(grayedMatch, grayedRuleInfo.getSpecialRule());
 
-		for (Node n : searchPlan) {
-			EObject ex = findExtension(resultMatch, graph, n);
-			
-			if (ex != null)
-				resultMatch.setNodeTarget(n, ex);
+		HashMap<Node, EObject> mapping = new HashMap<Node, EObject>();
+		for (Node n : grayedRuleInfo.getSpecialRule().getLhs().getNodes()) {
+			EObject x = grayedMatch.getNodeTarget(n);
+			if (x != null)
+				mapping.put(n, x);
 		}
 
-		resultMatch = GrayedRuleHandler.translateMatchFromGrayed(grayedRuleInfo, resultMatch);
+		Map<Node, EObject> extension = findExtension(mapping, graph, unboundNodeList, 0);
 
-		return resultMatch;
+		for (Entry<Node, EObject> entry : extension.entrySet()) {
+			grayedMatch.setNodeTarget(entry.getKey(), entry.getValue());
+		}
+		return GrayedRuleHandler.translateMatchFromGrayed(grayedRuleInfo, grayedMatch);
 	}
 
 	private List<Node> getUnboundNodes(Match resultMatch, Rule rule) {
@@ -80,7 +85,7 @@ public class LooseMatchFinder {
 		return result;
 	}
 
-	private EObject findExtension(Match currentMatch, EGraph graph, Node n) {
+	private EObject findExtension(Map<Node, EObject> currentMapping, EGraph graph, Node n) {
 		// Try to find an "anchor": a node that is connected to
 		// n and already has a match
 		Node anchor = null;
@@ -89,29 +94,29 @@ public class LooseMatchFinder {
 		boolean anchorIsSource = false;
 
 		for (Edge e : n.getAllEdges()) {
-			if (currentMatch.getNodeTarget(e.getSource()) != null) {
+			if (currentMapping.get(e.getSource()) != null) {
 				anchor = e.getSource();
 				anchorIsSource = true;
 				anchorEdge = e;
-				anchorObject = currentMatch.getNodeTarget(e.getSource());
+				anchorObject = currentMapping.get(e.getSource());
 				break;
 			}
 
-			if (currentMatch.getNodeTarget(e.getTarget()) != null) {
+			if (currentMapping.get(e.getTarget()) != null) {
 				anchor = e.getTarget();
 				anchorIsSource = false;
 				anchorEdge = e;
-				anchorObject = currentMatch.getNodeTarget(e.getTarget());
+				anchorObject = currentMapping.get(e.getTarget());
 				break;
 			}
 		}
 
 		List<EObject> candidateObjects = determineCandidates(graph, n, anchor, anchorEdge, anchorObject,
 				anchorIsSource);
-		candidateObjects.removeAll(currentMatch.getNodeTargets());
+		candidateObjects.removeAll(currentMapping.values());
 
 		for (EObject cand : candidateObjects) {
-			if (fulfillsConstraints(cand, n, currentMatch) == ConstraintCheckResult.PASSED) {
+			if (fulfillsConstraints(cand, n, currentMapping)) {
 				return cand;
 			}
 		}
@@ -119,39 +124,78 @@ public class LooseMatchFinder {
 		return null;
 	}
 
-	private ConstraintCheckResult fulfillsConstraints(EObject cand, Node n, Match resultMatch) {
+	private Collection<EObject> findExtensionCandidates(Map<Node, EObject> currentMatch, EGraph graph, Node n) {
+		// Try to find an "anchor": a node that is connected to
+		// n and already has a match
+		Node anchor = null;
+		Edge anchorEdge = null;
+		EObject anchorObject = null;
+		boolean anchorIsSource = false;
+
+		for (Edge e : n.getAllEdges()) {
+			if (currentMatch.get(e.getSource()) != null) {
+				anchor = e.getSource();
+				anchorIsSource = true;
+				anchorEdge = e;
+				anchorObject = currentMatch.get(e.getSource());
+				break;
+			}
+
+			if (currentMatch.get(e.getTarget()) != null) {
+				anchor = e.getTarget();
+				anchorIsSource = false;
+				anchorEdge = e;
+				anchorObject = currentMatch.get(e.getTarget());
+				break;
+			}
+		}
+
+		List<EObject> candidateObjects = determineCandidates(graph, n, anchor, anchorEdge, anchorObject,
+				anchorIsSource);
+		candidateObjects.removeAll(currentMatch.keySet());
+
+		Collection<EObject> result = new ArrayList<>();
+		for (EObject cand : candidateObjects) {
+			if (fulfillsConstraints(cand, n, currentMatch)) {
+				result.add(cand);
+			}
+		}
+		return result;
+	}
+
+	private boolean fulfillsConstraints(EObject cand, Node n, Map<Node, EObject> currentMatch) {
 		// minimalistic constraint checking..
 		// 1. no type-constraint check. these are already covered
 		// by the candidate selection process
 
 		// 2. edge constraint checks
 		for (Edge e : n.getOutgoing()) {
-			if (resultMatch.getNodeTarget(e.getTarget()) != null) {
-				EObject expected = resultMatch.getNodeTarget(e.getTarget());
+			if (currentMatch.get(e.getTarget()) != null) {
+				EObject expected = currentMatch.get(e.getTarget());
 				Object real = cand.eGet(e.getType()); // collection or 1 EObject
 				boolean passed = (expected == real)
 						|| ((real instanceof Collection) && ((Collection) real).contains(expected));
 				if (!passed) {
-					return ConstraintCheckResult.FAILED;
+					return false;
 				}
 			}
 		}
 		for (Edge e : n.getIncoming()) {
-			if (resultMatch.getNodeTarget(e.getSource()) != null) {
-				EObject source = resultMatch.getNodeTarget(e.getSource());
+			if (currentMatch.get(e.getSource()) != null) {
+				EObject source = currentMatch.get(e.getSource());
 				Object real = source.eGet(e.getType()); // collection or 1 EObject
 				boolean passed = (cand == real) || ((real instanceof Collection) && ((Collection) real).contains(cand));
 				if (!passed) {
-					return ConstraintCheckResult.FAILED;
+					return false;
 				}
 			}
 		}
-		
+
 		// dangling constraint checks
 		DanglingConstraint dc = retrieveDanglingConstraint(n);
 		if (!dc.check(cand, graph))
-			return ConstraintCheckResult.DANGLING_FAILED;
-		
+			return false;
+
 		// for now: only static values checked - attributes, ints, strings, floats
 		for (Attribute a : n.getAttributes()) {
 			String expectedValString = a.getValue();
@@ -172,14 +216,14 @@ public class LooseMatchFinder {
 			if (actualVal == null)
 				continue;
 			if (actualVal.equals(expectedVal))
-				return ConstraintCheckResult.FAILED;
+				return false;
 		}
 
-		return ConstraintCheckResult.PASSED;
+		return true;
 	}
 
 	private DanglingConstraint retrieveDanglingConstraint(Node n) {
-		Map<Node,DanglingConstraint> node2Constraint = new HashMap<>();
+		Map<Node, DanglingConstraint> node2Constraint = new HashMap<>();
 		DanglingConstraint entry = node2Constraint.get(n);
 		if (entry == null) {
 			Map<EReference, Integer> incoming = VariableInfo.getEdgeCounts(n, true);
@@ -228,7 +272,87 @@ public class LooseMatchFinder {
 	public void setParameter(String key, String value) {
 		Parameter param = rule.getParameter(key);
 		if (param != null)
-			paramValues.put(param,value);
+			paramValues.put(param, value);
+	}
+
+	public Map<Node, EObject> findExtension(Map<Node, EObject> currentMappings, EGraph graph,
+			List<Node> unboundNodeList, int position) {
+		Node n = unboundNodeList.get(position);
+
+		Collection<EObject> candidates = findExtensionCandidates(currentMappings, graph, n);
+		if (!candidates.isEmpty()) {
+//			 1. wiederhole, solange es noch neue gültige Teil-Lösungsschritte gibt:
+//		     a) wähle einen neuen gültigen Teil-Lösungsschritt
+			for (EObject x : candidates) {
+//		     b) falls Wahl gültig ist:
+//            I) erweitere Vektor um Wahl;
+				Map<Node, EObject> currentMappingsExtended = new HashMap<>(currentMappings);
+				currentMappingsExtended.put(n, x);
+
+//           II) falls Vektor vollständig ist, return true; // Lösung gefunden!
+				if (position == unboundNodeList.size() - 1) {
+					return currentMappingsExtended;
+				}
+//               sonst: falls (FindeLösung(Stufe+1, Vektor)) return true; // Lösung!
+				else {
+					Map<Node, EObject> nextSolution = findExtension(currentMappingsExtended, graph, unboundNodeList,
+							position + 1);
+					if (nextSolution != null && checkDanglingPostponed(n, graph, nextSolution, unboundNodeList)) {
+						return nextSolution;
+					} else {
+//	                    sonst mache Wahl rückgängig; // Sackgasse (Backtracking)!
+						currentMappingsExtended.remove(n);
+					}
+				}
+
+			}
+		} else { // d.h., falls candidates == empty
+			if (position == unboundNodeList.size() - 1) {
+				return currentMappings;
+			} else {
+				return findExtension(currentMappings, graph, unboundNodeList, position + 1);
+			}
+		}
+
+		// 2. Da es keinen neuen Teil-Lösungsschritt gibt: return false // Keine Lösung!
+		return null;
+	}
+
+	private boolean checkDanglingPostponed(Node n, EGraph graph, Map<Node, EObject> nextSolution,
+			List<Node> unboundNodeList) {
+		DanglingConstraint dc = retrieveDanglingConstraint(n);
+
+		// adapt edge counts
+		Map<EReference, Integer> removedIncoming = new HashMap<>();
+		Map<EReference, Integer> removedOutgoing = new HashMap<>();
+
+		for (Edge e : n.getIncoming()) {
+			if (nextSolution.get(e.getSource()) == null) {
+				dc.increaseIncoming(e.getType(), -1);
+				if (removedIncoming.containsKey(e.getType()))
+					removedIncoming.put(e.getType(), 1 + removedIncoming.get(e.getType()));
+				else
+					removedIncoming.put(e.getType(), 1);
+			}
+		}
+		for (Edge e : n.getOutgoing()) {
+			if (nextSolution.get(e.getTarget()) == null) {
+				dc.increaseOutgoing(e.getType(), -1);
+				if (removedOutgoing.containsKey(e.getType()))
+					removedOutgoing.put(e.getType(), 1 + removedOutgoing.get(e.getType()));
+				else
+					removedOutgoing.put(e.getType(), 1);
+			}
+		}
+
+		boolean passed = dc.check(nextSolution.get(n), graph);
+
+		// restore original edge counts
+		for (Entry<EReference, Integer> entry : removedIncoming.entrySet())
+			dc.increaseIncoming(entry.getKey(), entry.getValue());
+		for (Entry<EReference, Integer> entry : removedOutgoing.entrySet())
+			dc.increaseOutgoing(entry.getKey(), entry.getValue());
+		return passed;
 	}
 
 }
